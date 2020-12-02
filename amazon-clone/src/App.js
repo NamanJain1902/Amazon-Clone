3bb3566fc6ef53ae8b8d75c0e3e1638cd09e08d8
nj1902 <1902ben10@gmail.com>
2020-12-02 18:22:01 +0530

    router-setup

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import Header from './Header';  // imported hook->header
 import Home from "./Home";
 
 function App() {
   return (
     // BEM convention(naming convention to use for styling)
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
-
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I'm Checkout Smash bang.!!!</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
