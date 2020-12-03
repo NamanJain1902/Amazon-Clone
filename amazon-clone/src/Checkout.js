3f5d7f82009fbedfb9e7a8555cd7eb3499d5bac5
nj1902 <1902ben10@gmail.com>
2020-12-03 13:55:11 +0530

    data-layer added

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Checkout.css'
+import Subtotal from './Subtotal'
 function Checkout() {
     return (
         <div className="checkout">
@@ -14,7 +15,7 @@ function Checkout() {
             </div>
 
             <div className="checkout__right">
-                <h1>Subtotal</h1>
+                <Subtotal />
             </div>
         </div>
     )
--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import './Product.css'
 function Product({title, image, price, rating}) {
+
+    const [{basket}, dispatch] = useStateValue();
+    const addToBasket = () {
+        // dispatch item to data layer
+        dispatch({
+            type : 'ADD_TO_BASKET',
+            item : {
+                id : id,
+                title : title,
+                image : image,
+                price : price,
+                rating : rating,
+            }
+        });
+    }
+
     return (
         <div className='product'> 
 
@@ -19,7 +35,7 @@ function Product({title, image, price, rating}) {
 
             <img src={image} />
 
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
